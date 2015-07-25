98b24c0241c2617aec06c588d2f430fe59f87812
Max Harris <harris.max@gmail.com>
2015-07-25 10:31:43 -0700

    added setDrawMode hack open the browser console and run setDrawMode() to start drawing, then run setDrawMode() to stop drawing

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var Geometry = require('gl-geometry')
-var fit      = require('canvas-fit')
-var glShader = require('gl-shader')
-var mat4     = require('gl-mat4')
-var normals  = require('normals')
-var glslify  = require('glslify')
+var Geometry = require('gl-geometry');
+var fit      = require('canvas-fit');
+var glShader = require('gl-shader');
+var mat4     = require('gl-mat4');
+var normals  = require('normals');
+var glslify  = require('glslify');
 //var bunny    = require('bunny')
 
 var dot = require('gl-vec3/dot');
@@ -12,35 +12,31 @@ var pick = require('camera-picking-ray');
 //var intersect = require('ray-sphere-intersection');
 var intersect = require('ray-plane-intersection');
 
-
 //var radius = 1;
 // var bunny = require('primitive-sphere')(radius, {
 //   segments: 16
 // });
 
-var bunny = require('primitive-quad')()
+var bunny = require('primitive-quad')();
 
 // Creates a canvas element and attaches
 // it to the <body> on your DOM.
-var canvas = document.body.appendChild(document.createElement('canvas'))
+var canvas = document.body.appendChild(document.createElement('canvas'));
 
 // Creates an instance of canvas-orbit-camera,
 // which later will generate a view matrix and
 // handle interaction for you.
-var camera = require('canvas-orbit-camera')(canvas)
+var camera = require('canvas-orbit-camera')(canvas);
 
 // A small convenience function for creating
 // a new WebGL context – the `render` function
 // supplied here is called every frame to draw
 // to the screen.
-var gl = require('gl-context')(canvas, render)
+var gl = require('gl-context')(canvas, render);
 
 // Resizes the <canvas> to fully fit the window
 // whenever the window is resized.
-window.addEventListener('resize'
-  , fit(canvas)
-  , false
-)
+window.addEventListener('resize', fit(canvas), false);
 
 // Load the bunny mesh data (a simplicial complex)
 // into a gl-geometry instance, calculating vertex
@@ -50,17 +46,15 @@ window.addEventListener('resize'
 // with three.js, this is essentially equivalent to an array
 // of `THREE.Vector3` and `THREE.Face3` instances, except specified
 // as arrays for simplicity and interoperability.
-var geometry = Geometry(gl)
+var geometry = Geometry(gl);
 
-geometry.attr('aPosition', bunny.positions)
+geometry.attr('aPosition', bunny.positions);
 geometry.attr('aNormal', normals.vertexNormals(
-    bunny.cells     //  [[1, 2, 3], [3, 4, 5]]
-  , bunny.positions // [[0, 0, 0], [1, 0, 0]]
-))
+  bunny.cells,    // [[1, 2, 3], [3, 4, 5]]
+  bunny.positions // [[0, 0, 0], [1, 0, 0]]
+));
 
-
-
-geometry.faces(bunny.cells)
+geometry.faces(bunny.cells);
 
 // PSLG (Planar straight-line graphs)
 // [[1, 2], [2, 3], [3, 1]]
@@ -69,20 +63,21 @@ geometry.faces(bunny.cells)
 var sketch = {
   positions: [[-1, -1, 0], [1, 1, 0]],
   cells: [[0, 1]]
-}
+};
+
 var sketchGeometry = Geometry(gl);
 
-sketchGeometry.attr('aPosition', sketch.positions)
-sketchGeometry.faces(sketch.cells, { size: 2 })
+sketchGeometry.attr('aPosition', sketch.positions);
+sketchGeometry.faces(sketch.cells, { size: 2 });
 
 // Create the base matrices to be used
 // when rendering the bunny. Alternatively, can
 // be created using `new Float32Array(16)`
-var projection = mat4.create()
-var model      = mat4.create()
-var view       = mat4.create()
-var height
-var width
+var projection = mat4.create();
+var model      = mat4.create();
+var view       = mat4.create();
+var height;
+var width;
 
 // Pulls up our shader code and returns an instance
 // of gl-shader. Using the glslify browserify transform,
@@ -93,15 +88,17 @@ var width
 // savings by doing this in Node rather then at runtime in
 // the browser.
 
-var drawPlaneShader = glShader(gl,
-    glslify('./shaders/bunny.vert')
-  , glslify('./shaders/bunny.frag')
-)
+var drawPlaneShader = glShader(
+  gl,
+  glslify('./shaders/bunny.vert'),
+  glslify('./shaders/bunny.frag')
+);
 
-var sketchShader = glShader(gl,
-    glslify('./shaders/sketch.vert')
-  , glslify('./shaders/sketch.frag')
-)
+var sketchShader = glShader(
+  gl,
+  glslify('./shaders/sketch.vert'),
+  glslify('./shaders/sketch.frag')
+);
 
 // The logic/update loop, which updates all of the variables
 // before they're used in our render function. It's optional
@@ -109,38 +106,39 @@ var sketchShader = glShader(gl,
 function update() {
   // Updates the width/height we use to render the
   // final image.
-  width  = gl.drawingBufferWidth
-  height = gl.drawingBufferHeight
+  width  = gl.drawingBufferWidth;
+  height = gl.drawingBufferHeight;
 
   // Updates the view matrix via the camera state
-  camera.view(view)
+  camera.view(view);
 
   // Optionally, flush the state of the camera. Required
   // for user input to work correctly.
-  camera.tick()
+  camera.tick();
 
   // Update our projection matrix. This is the bit that's
   // responsible for taking 3D coordinates and projecting
   // them into 2D screen space.
-  var aspectRatio = gl.drawingBufferWidth / gl.drawingBufferHeight
-  var fieldOfView = Math.PI / 4
-  var near = 0.01
-  var far  = 100
+  var aspectRatio = gl.drawingBufferWidth / gl.drawingBufferHeight;
+  var fieldOfView = Math.PI / 4;
+  var near = 0.01;
+  var far  = 100;
 
-  mat4.perspective(projection
-    , fieldOfView
-    , aspectRatio
-    , near
-    , far
-  )
+  mat4.perspective(
+    projection,
+    fieldOfView,
+    aspectRatio,
+    near,
+    far
+  );
 }
 
 function render() {
-  update()
+  update();
 
   // Sets the viewport, i.e. tells WebGL to draw the
   // scene across the full canvas.
-  gl.viewport(0, 0, width, height)
+  gl.viewport(0, 0, width, height);
 
   // Enables depth testing, which prevents triangles
   // from overlapping.
@@ -152,18 +150,18 @@ function render() {
 
   // Binds the geometry and sets up the shader's attribute
   // locations accordingly.
-  geometry.bind(drawPlaneShader)
+  geometry.bind(drawPlaneShader);
 
   // Updates our model/view/projection matrices, sending them
   // to the GPU as uniform variables that we can use in
   // `shaders/bunny.vert` and `shaders/bunny.frag`.
-  drawPlaneShader.uniforms.uProjection = projection
-  drawPlaneShader.uniforms.uView = view
-  drawPlaneShader.uniforms.uModel = model
+  drawPlaneShader.uniforms.uProjection = projection;
+  drawPlaneShader.uniforms.uView = view;
+  drawPlaneShader.uniforms.uModel = model;
 
   // Finally: draws the bunny to the screen! The rest is
   // handled in our shaders.
-  geometry.draw(gl.TRIANGLES)
+  geometry.draw(gl.TRIANGLES);
 
 
 
@@ -172,34 +170,18 @@ function render() {
 
   // Binds the geometry and sets up the shader's attribute
   // locations accordingly.
-  sketchGeometry.bind(sketchShader)
+  sketchGeometry.bind(sketchShader);
 
 
   // Updates our model/view/projection matrices, sending them
   // to the GPU as uniform variables that we can use in
   // `shaders/bunny.vert` and `shaders/bunny.frag`.
-  sketchShader.uniforms.uProjection = projection
-  sketchShader.uniforms.uView = view
-  sketchShader.uniforms.uModel = model
-  gl.lineWidth(2)
-  sketchGeometry.draw(gl.LINES)
+  sketchShader.uniforms.uProjection = projection;
+  sketchShader.uniforms.uView = view;
+  sketchShader.uniforms.uModel = model;
+  gl.lineWidth(2);
+  sketchGeometry.draw(gl.LINES);
 }
-
-// clientX: 542
-// clientY: 150
-// layerX: 542
-// layerY: 150
-// offsetX: 542
-// offsetY: 150
-// pageX: 542
-// pageY: 150
-// screenX: 871
-// screenY: 212
-// x: 542
-// y: 150
-// ['mousedown', 'mouseup', 'click', 'mousewheel'].forEach(function(name) {
-  gl.canvas.addEventListener('mousemove', handleMouseMove, true);
-// });
 
 // function handleMouse (event) {
 //   //console.log('click', event.x, event.y);
@@ -229,12 +211,12 @@ function render() {
 // }
 
 function updateSketchGeometry(position, mouse3) {
-console.log('mouse3:', mouse3)
+console.log('mouse3:', mouse3);
   position[0] = mouse3[0];
   position[1] = mouse3[1];
   position[2] = mouse3[2];
 
-console.log("cells:", sketch.cells.join(';'), 'position length:', sketch.positions.length)
+console.log('cells:', sketch.cells.join(';'), 'position length:', sketch.positions.length);
   // TODO: this is a horrible hack!
   //sketchGeometry.dispose()
   sketchGeometry._attributes.length = 0;
@@ -255,35 +237,60 @@ function projectMouseToPlane(event) {
   var mouse = [event.x, event.y];
   var viewport = [0, 0, width, height];
 
-  var projView = mat4.multiply([], projection, view)
+  var projView = mat4.multiply([], projection, view);
   var invProjView = mat4.invert([], projView);
 
   pick(ray.origin, ray.direction, mouse, viewport, invProjView);
 
-  var center = [0, 0, 0];
+  //var center = [0, 0, 0];
   // TODO: compute the actual plane normal
   var planeNormal = [0, 0, -1];
   return intersect(out, ray.origin, ray.direction, planeNormal, -dot(planeNormal, [1, 0, 0]));
 }
 
+gl.canvas.addEventListener('click', handleMouseClick, true);
+gl.canvas.addEventListener('mousemove', handleMouseMove, true);
 
-gl.canvas.addEventListener('click', function handleMouseClick(ev) {
-  var mouse3 = projectMouseToPlane(event)
+function handleMouseClick(event) {
+  if (!drawMode) { return; }
+
+  var mouse3 = projectMouseToPlane(event);
 
   if (mouse3) {
     var length = sketch.positions.length;
-    var last = sketch.cells[sketch.cells.length - 1]
-    sketch.positions.push(mouse3)
+    var last = sketch.cells[sketch.cells.length - 1];
+    sketch.positions.push(mouse3);
     sketch.cells.push([last[1], last[1] + 1]);
-    console.log(sketch)
-    updateSketchGeometry(sketch.positions[sketch.positions.length - 1], mouse3)
-  }
-}, true);
-
-function handleMouseMove (event) {
-  var mouse3 = projectMouseToPlane(event)
-  if (mouse3) {
-    var last = sketch.positions[sketch.positions.length - 1]
-    updateSketchGeometry(last, mouse3)
+    console.log(sketch);
+    updateSketchGeometry(sketch.positions[length - 1], mouse3);
   }
 }
+
+function handleMouseMove (event) {
+  if (!drawMode) { return; }
+
+  var mouse3 = projectMouseToPlane(event);
+  if (mouse3) {
+    var last = sketch.positions[sketch.positions.length - 1];
+    updateSketchGeometry(last, mouse3);
+  }
+}
+
+var drawMode = false;
+window.setDrawMode = function () {
+  drawMode = !drawMode;
+  console.log('setting drawMode:', drawMode);
+};
+
+/*
+/// externals:
+
+the plane,
+matrices,
+PSLG management:
+  - a way to add points,
+  - remove points,
+  - move points,
+  - close the path (by using topology: sketch.cells) - by typing a command in the console `closePath()`
+  // https://github.com/mikolalysenko/simplicial-complex might help
+*/
