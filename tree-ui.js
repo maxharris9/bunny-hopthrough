29e4dd36fbe5842d27a7b848a09dc2fa4cb42571
Max Harris <harris.max@gmail.com>
2015-09-20 11:15:29 -0700

    added tree-ui.js

---
+++ tree-ui.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export class Tree extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+    }
+  }
+
+  render () {
+    return (<div>Hey hey hey!</div>)
+  }
+}
+
+class CsgNode extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  render () {
+    return (<div></div>)
+  }
+}
